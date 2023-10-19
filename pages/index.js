12b1288b20104aa0ab263a6e53588802b32e5228
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-10-19 14:23:14 +0300

    deplyment

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Header from '@/components/Header'
 import RecentOrders from '@/components/RecentOrders'
 import TopCards from '@/components/TopCards'
 import Image from 'next/image'
-
+import {AiOutlineCopyrightCircle} from 'react-icons/ai'
 
 export default function Home() {
   return (
@@ -13,6 +13,10 @@ export default function Home() {
     <div className='p-4 grid md:grid-cols-3 grid-cols-1 gap-4'>
       <Barchat />
       <RecentOrders />
+    </div>
+    <div className='flex justify-center items-center mb-2 bg-gray-200 p-3 rounded-lg  '>
+      <AiOutlineCopyrightCircle />  
+      <h3 className='font-bold text-gray-600'>Developer Evan Kimani@2023</h3>
     </div>
     </main>
   )
